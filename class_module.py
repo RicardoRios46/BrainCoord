e26656461610ad285cb5f491ddf121f98ed16102
Ricardo Rios <ricardo.rios@comunidad.unam.mx>
2020-01-29 12:16:53 -0600

    Commin Inicial. Tiene el prototipo de clase

---
+++ class_module.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Nucleo:
+    database_filename = "nombre del archivo"
+    reference_point = None  # lambda o bregma
+    n_points = 1
+    coordinate0 = [0, 0, 0]
+
+    lambda_range = np.zeros((n_points, 1))
+    bregma_range = np.zeros((n_points, 1))
+    medioLateral_range = np.zeros((n_points, 2))
+    dorsoVentral_range = np.zeros((n_points, 2))
+
+    def read_list(self):
+        print("carga la lista")
+
+    def create_point(self):
+        print("crea el punto intermedio a paritr de la lista")
+
+    def get_coordinates(self):
+        print("")
+
+    def update_bitacore(self):
+        print("Actualizar e impimir bitacora")
