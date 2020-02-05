f6f796bcaf3bf3f178ea35aecea1bac9743a747a
Ricardo Rios <ricardo.rios@unam.comunidad.mx>
2020-02-05 15:57:54 -0600

    Actualizacion de formulas y tabla de prueba

--- class_module.py
+++ class_module.py
@@ -2,16 +2,18 @@ import numpy as np
 
 
 class Nucleo():
-    #Predefinimos inputs
+    # Atributos publicos de los inputs.
     database_filename = "nombre del archivo"
     reference_point = None  # lambda o bregma
     coordinate0 = [0, 0, 0]
 
-    # Estas variables contendran columnas con la info de las coordenadas
+    # Atributos publicos. Estas variables contendran columnas con la info de las coordenadas
     lambda_range = None
     bregma_range = None
-    medioLateral_range = None
-    dorsoVentral_range = None
+    medio_range = None
+    lateral_range = None
+    dorso_range = None
+    ventral_range = None
     n_points = 0
 
     def __init__(self, database_filename, reference_point, coordinate0):
@@ -19,25 +21,59 @@ class Nucleo():
         self.reference_point = reference_point
         self.coordinate0 = coordinate0
 
+        # Atributos privados
+        self.__xRange = None
+        self.__yRange = None
+        self.__area = None
+        self.__cuadro_chido = 0
+
     def read_list(self):
-        dataset = np.genfromtxt(self.database_filename, skip_header=1, delimiter=",", usecols=(range(6))) # Cargamos el .csv, no tomamos el header y solo carmaos las primeras 5 columnas
-        dataset = dataset[~np.isnan(dataset).any(axis=1)] # Eliminamos las filas con NaNs
+        """ Docummenting function blah blah blah"""
+        # ToDo revisar el caso cuando solo hay una fila
+        dataset = np.genfromtxt(self.database_filename, skip_header=1, delimiter=",", usecols=(
+            range(6)))  # Cargamos el .csv, no tomamos el header y solo carmaos las primeras 5 columnas
+        dataset = dataset[~np.isnan(dataset).any(axis=1)]  # Eliminamos las filas con NaNs
+        print(dataset[3,:])
         # Rellenamos las variables del objeto con la info de la tabla
-        self.lambda_range = dataset[:,0]
-        self.bregma_range = dataset[:,1]
-        self.medioLateral_range = dataset[:,2:4]
-        self.dorsoVentral_range = dataset[:,4:6]
+        self.lambda_range = dataset[:, 0]
+        self.bregma_range = dataset[:, 1]
+        self.lateral_range = dataset[:, 2]
+        self.medio_range = dataset[:, 3]
+        self.dorso_range = dataset[:, 4]
+        self.ventral_range = dataset[:, 5]
         # El numero total de filas
         self.n_points = self.bregma_range.size
 
-        print("carga la lista")
-
     def create_point(self):
-        print("crea el punto intermedio a paritr de la lista")
+        # Obtenemos las ditancias de los cuadrados en la base de datos
+        self.__xRange = self.medio_range - self.lateral_range
+        self.__yRange = self.ventral_range - self.dorso_range
+        # Obtenemos el area de los cuadrados en la base de datos
+        self.__area = abs(self.__xRange * self.__yRange)
+        self.__cuadro_chido = self.__area.argmax()
+
+        # Prints para debuug
+        print("indice cuadro chido= ", self.__cuadro_chido)
+        print("xRange=", self.__xRange[self.__cuadro_chido])
+        print("yRange=",self.__yRange[self.__cuadro_chido])
+        print("area=" + str(self.__area[self.__cuadro_chido]))
 
     def get_coordinates(self):
-        print("")
-        return [0, 0, 0]
+        if self.reference_point == "lambda":
+            ap = self.coordinate0[0] + self.lambda_range[self.__cuadro_chido]
 
+        else:
+            ap = self.coordinate0[0] + self.bregma_range[self.__cuadro_chido]
+        ml = self.coordinate0[1] + self.__xRange[self.__cuadro_chido] / 2
+        vd = self.coordinate0[2] - (self.__yRange[self.__cuadro_chido] / 2 + self.dorso_range[self.__cuadro_chido])
+
+        # Prints para debug
+        print("lambda_range=", self.lambda_range[self.__cuadro_chido])
+        print("x_range/2=", self.__xRange[self.__cuadro_chido] / 2)
+        print("y_range/2=", self.__yRange[self.__cuadro_chido] / 2)
+        # ToDo Redondear a 2 decimales
+        return [round(ap,1), round(ml,1), round(vd,1)]
+
+    # ToDo Implementar una bitacora
     def update_bitacore(self):
         print("Actualizar e impimir bitacora")
