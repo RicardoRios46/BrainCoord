146e301b80b40f8c509a1d51bdfc0fefc4306462
Gabriela Gonzalez <gabebyre@gmail.com>
2020-02-06 13:01:23 -0600

    Script docstring

--- BrainCoord.py
+++ BrainCoord.py
@@ -1,8 +1,27 @@
+"""BrainCoord 
+
+It was built in reference to the Mouse Brain Paxinos Atlas. This script allows to calculate the optimal coordinates to reach any brain nucleus in the mouse, giving the resultant coordinates points (AP, ML, DV) from a coordenate 0 (bregma or lambda), previously entered by the user. In addition this script is open to import datbase of any particular nucleus. 
+
+This tool accepts .cvs files. Which requires to contain the following ordered coordinates (in mm), per each imported nucleus: 
+    * point reference (bregma/lambda) 
+    * lateral limit 
+    * medial limit
+    * dorsal limit
+    * vetral limit
+    
+This script requires that `NumPy` be installed within the actual Python environment.
+
+"""
+
+
 import class_module
 import numpy as np
 
 # ToDo ESta funcion hara testing del formato de los inputs. Va a estar decorada con Click para las entradas desde terminal
 def test_inputs(database_filename, reference_point, coordinate0):
+    
+    
+    
     #database_filename = "nombre del archivo"
     #reference_point = None  # lambda o bregma
     #coordinate0 = 0
