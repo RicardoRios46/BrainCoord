1b70c6b972d6ff6e41d55391537f92586e82033c
Ricardo Rios <ricardo.rios@unam.comunidad.mx>
2020-02-06 15:58:21 -0600

    Las coordenadas solo son floats. Corrigiendo ejecucion para click

--- BrainCoord.py
+++ BrainCoord.py
@@ -6,34 +6,21 @@ import click
 @click.command()
 @click.option('--database_filename', prompt = 'Introduce your text file name within the extention')
 @click.option('--reference_point',  prompt = 'Introduce you reference point (bregma or lambda)')
-@click.option('--AP', prompt = 'Introduce AP coordinate')
-@click.option('--ML', prompt = 'Introduce ML coordinate')
-@click.option('--DV', prompt = 'Introduce DV coordinate')
-def getCheck_inputs(database_filename, reference_point, AP, ML, DV):
-    coordinate0 = [AP, ML, DV]
+@click.option('--ap', prompt = 'Introduce AP coordinate',type=float)
+@click.option('--ml', prompt = 'Introduce ML coordinate',type=float)
+@click.option('--dv', prompt = 'Introduce DV coordinate',type=float)
+def brain_coord(database_filename, reference_point, ap, ml, dv):
+    coordinate0 = [ap, ml, dv]
+
     chI.checkFormat_referece_point(reference_point)
     chI.checkFormat_coordinate0(coordinate0)
     chI.checkValues_coordinate0(coordinate0, reference_point)
     chI.checkExistence_file(database_filename)
     chI.checkFormat_file(database_filename)
 
-    return [database_filename, reference_point, coordinate0]
-
-def test_inputs(database_filename, reference_point, coordinate0):
-    click.echo("Your inputs are: " + database_filename + " " + reference_point +  " " +coordinate0)
-
-
-
-if __name__ == '__main__':
-    #test_inputs()
-    # Inputs de prueba
-    inputs = getCheck_inputs()
     #database_filename = "medial_der.csv"
     #reference_point = "lambda"
     #coordinate0 = [33, 15, 63.7]
-    database_filename = inputs[0]
-    reference_point = inputs[1]
-    coordinate0 = inputs[2]
     print(coordinate0)
     #test_inputs(database_filename, reference_point, coordinate0)
 
@@ -45,3 +32,8 @@ if __name__ == '__main__':
     coordinates_result = nucleo.get_coordinates()
 
     print(coordinates_result)
+
+
+if __name__ == '__main__':
+    brain_coord()
+
